f44cb1899b1a866de0e0b15e1cb80397873f4af0
LuanPeixotop <luanpeixotopereira@gmail.com>
2018-03-04 22:35:21 -0300

    Adicionando detalhes sobre os livros e linkando na lista

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -42,7 +43,7 @@ public class ProdutosController {
 	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult bindingResult,
 			RedirectAttributes redirectAttributes) {
 		
-		String path = fileSaver.write("arquivos-sumario", sumario);
+		String path = fileSaver.write("arquivos", sumario);
 	    produto.setSumarioPath(path);
 
 		if (bindingResult.hasErrors()) {
@@ -60,6 +61,14 @@ public class ProdutosController {
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
 		modelAndView.addObject("produtos", produtos);
 		return modelAndView;
+	}
+	
+	@RequestMapping("/detalhe/{id}")
+	public ModelAndView detalhe(@PathVariable("id") Integer id){
+	    ModelAndView modelAndView = new ModelAndView("/produtos/detalhe");
+	    Produto produto = produtoDao.find(id);
+	    modelAndView.addObject("produto", produto);
+	    return modelAndView;
 	}
 
 	@InitBinder
