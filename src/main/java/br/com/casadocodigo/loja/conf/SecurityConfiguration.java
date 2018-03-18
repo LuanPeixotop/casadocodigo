79261eb200f95aebef741a7400f3602901f68864
LuanPeixotop <luanpeixotopereira@gmail.com>
2018-03-18 20:49:53 -0300

    Implementado esquema de login do SpringSecurity

---
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.casadocodigo.loja.daos.UsuarioDao;
+
+@SuppressWarnings("deprecation")
+@EnableWebMvcSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UsuarioDao usuarioDao;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/produtos/form").hasRole("ADMIN")
+		.antMatchers("/carrinho").permitAll()
+		.antMatchers("/produtos").hasRole("ADMIN")
+		.antMatchers("/produtos/**").permitAll()
+		.antMatchers("/").permitAll()
+		.anyRequest().authenticated().and().formLogin();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioDao)
+			.passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+	
+	
+	
+	
+}	
